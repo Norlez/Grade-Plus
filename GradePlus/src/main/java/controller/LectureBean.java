49f4b4fe837d3b2f291c6df96f3a236c38a6d8c3
Torben Groß <grosst@uni-bremen.de>
2018-02-02 19:28:56 +0100

    LectureBean

--- GradePlus/src/main/java/controller/LectureBean.java
+++ GradePlus/src/main/java/controller/LectureBean.java
@@ -2,13 +2,18 @@ package controller;
 
 import common.model.Lecture;
 import common.model.Session;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import persistence.LectureDAO;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.List;
+
+import static common.util.Assertion.assertNotNull;
 
 /**
  * Dieses Bean verwaltet Lehrveranstaltungen.
@@ -21,16 +26,54 @@ import java.util.List;
 public class LectureBean extends AbstractBean implements Serializable {
 
     /**
+     * Der Logger für diese Klasse.
+     */
+    private static final Logger logger = Logger.getLogger(UsersBean.class);
+
+    /**
+     * Das Data-Access-Objekt, das die Verwaltung der Persistierung für
+     * Lehrveranstaltungs-Objekte übernimmt.
+     */
+    private final LectureDAO lectureDao;
+
+    /**
+     * Die aktuell angezeigte Lehrveranstaltung, dessen Attribute durch die
+     * UIKomponenten des Facelets geschrieben und gelesen werden.
+     */
+    private Lecture lecture;
+
+    /**
+     * Die Liste aller innerhalb der Applikation bekannten Lehrveranstaltungen.
+     */
+    private List<Lecture> allLectures;
+
+    /**
      * Erzeugt eine neue LecturesBean.
      *
      * @param pSession
      *            Die Session der zu erzeugenden LecturesBean.
+     * @param pLectureDao
+     *            Die Session der zu erzeugenden LecturesBean.
+     *
      * @throws IllegalArgumentException
      *             Falls {@code pSession} {@code null} ist.
      */
     @Inject
-    public LectureBean(Session pSession, LectureDAO lectureDAO) {
+    public LectureBean(Session pSession, LectureDAO pLectureDao) {
         super(pSession);
+        lectureDao = assertNotNull(pLectureDao);
+    }
+
+    /**
+     * Initialisiert die Attribute {@link #lecture} und {@link #allLectures},
+     * sodass {@link #lecture} eine neu anzulegende {@link Lecture} repräsentiert
+     * und {@link #allLectures} alle bekannten Lehrveranstaltungen der Applikation
+     * enthält.
+     */
+    @PostConstruct
+    public void init() {
+        lecture = new Lecture();
+        allLectures = lectureDao.getAllLectures();
     }
 
     /**
@@ -39,7 +82,7 @@ public class LectureBean extends AbstractBean implements Serializable {
      * @return Die anzuzeigende Lehrveranstaltung.
      */
     public Lecture getLecture() {
-        throw new UnsupportedOperationException();
+        return lecture;
     }
 
     /**
@@ -49,31 +92,59 @@ public class LectureBean extends AbstractBean implements Serializable {
      *         Lehrveranstaltung.
      */
     public List<Lecture> getAllLectures() {
-        throw new UnsupportedOperationException();
+        return allLectures;
     }
 
     /**
      * Fügt die aktuell angezeigte Lehrveranstaltung der Liste aller innerhalb der
      * Applikation bekannten Lehrveranstaltungen hinzu.
+     *
+     * @return "lectures.xhtml", um auf das Facelet der Übersicht der Lehrveranstaltung
+     * zu leiten.
      */
-    public void save() {
-        throw new UnsupportedOperationException();
+    public String save() {
+        try {
+            lectureDao.save(lecture);
+        } catch (final IllegalArgumentException e) {
+            addErrorMessageWithLogging(e, logger, Level.DEBUG,
+                    getTranslation("errorLecturedataIncomplete"));
+        }
+        return "lectures.xhtml";
     }
 
     /**
      * Aktualisiert die aktuell angezeigte Lehrveranstaltung in der Liste aller innerhalb
      * der Applikation bekannten Lehrveranstaltungen.
+     *
+     * @return "lectures.xhtml", um auf das Facelet der Übersicht der Lehrveranstaltung
+     * zu leiten.
      */
-    public void update() {
-        throw new UnsupportedOperationException();
+    public String update() {
+        try {
+            lectureDao.update(lecture);
+        } catch (final IllegalArgumentException e) {
+            addErrorMessageWithLogging(e, logger, Level.DEBUG,
+                    getTranslation("errorLecturedataIncomplete"));
+        }
+        return "lectures.xhtml";
     }
 
     /**
-     * Entfernt die aktuell angezeigte Lehrveranstaltung aus der Liste aller innerhalb der
-     * Applikation bekannten Lehrveranstaltungen.
+     * Entfernt die übergebene Lehrveranstaltung aus der Liste aller bekannten
+     * Lehrveranstaltung unter Verwendung des entsprechenden Data-Access-Objekts.
+     * Sollte die zu entfernende Lehrveranstaltung nicht in der Liste der
+     * Lehrveranstaltung vorhanden sein, passiert nichts.
+     *
+     * @param pLecture
+     *            Die zu entfernende Lehrveranstaltung.
+     * @return {code null} damit nicht zu einem anderen Facelet navigiert wird.
+     * @throws IllegalArgumentException
+     *             Falls die übergebene Lehrveranstaltung den Wert {@code null} hat.
      */
-    public void remove() {
-        throw new UnsupportedOperationException();
+    public String remove(final Lecture pLecture) {
+        lectureDao.remove(pLecture);
+        init();
+        return null;
     }
 
 }
