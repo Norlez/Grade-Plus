9331988ca9d19e0e2948a136d9a688e6af04f31d
arbnor <arbnor@uni-bremen.de>
2018-03-08 20:28:22 +0100

    Andis Dokumenten erstellungs methode in documentBean hinzugefügt, noch nichts in xhtml

--- GradePlus/src/main/java/controller/DocumentBean.java
+++ GradePlus/src/main/java/controller/DocumentBean.java
@@ -1,13 +1,22 @@
 package controller;
 
 import common.model.*;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 import persistence.ExamDAO;
 import persistence.UserDAO;
 
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.Serializable;
+import javax.servlet.ServletContext;
+import java.io.*;
 
 import static common.util.Assertion.*;
 
@@ -53,5 +62,44 @@ public class DocumentBean extends AbstractBean implements Serializable {
     public String getMatrNrOfStudent(User pStudent) {
         assertNotNull(pStudent);
         return userDAO.getUserForUsername(pStudent.getUsername()).getMatrNr();
+    }
+
+
+    /**
+     * Methode zum Drucken des Protokolls
+     *
+     */
+    public StreamedContent getProtocol () throws IOException {
+
+        String relativeWebPath = ((ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext()).getRealPath("/resources/") + "/documents/Protocol.pdf";
+        File file = new File(relativeWebPath);
+
+        try {
+
+            PDDocument document = PDDocument.load(file);
+            PDPage page = document.getPage(0);
+
+            PDFont fontTimes = PDType1Font.TIMES_ROMAN;
+
+            PDPageContentStream contentStream = new PDPageContentStream(document, page, true, true, true);
+
+            contentStream.beginText();
+            contentStream.setFont(fontTimes,12);
+            contentStream.newLineAtOffset(80,500);
+            contentStream.showText("Hello again");
+            contentStream.endText();
+
+            contentStream.close();
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            document.save(outputStream);
+            document.close();
+
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+
+            return new DefaultStreamedContent(inputStream, "application/pdf", "Protocol.pdf");
+
+        } catch (IOException e) {
+            throw new IOException("File not saved");
+        }
     }
 }
