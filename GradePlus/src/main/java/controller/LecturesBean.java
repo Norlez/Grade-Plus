2bb57c2272dea9107d0ce4e4e4f3101d4f7abfe5
Torben Groß <grosst@uni-bremen.de>
2018-03-08 10:33:38 +0100

    semester.xhtml: Anzeige korrigiert

--- GradePlus/src/main/java/controller/LecturesBean.java
+++ GradePlus/src/main/java/controller/LecturesBean.java
@@ -86,6 +86,15 @@ public class LecturesBean extends AbstractBean implements Serializable {
     }
 
     /**
+     * Gibt die anzuzeigende Lehrveranstaltung zurück.
+     *
+     * @return Die anzuzeigende Lehrveranstaltung.
+     */
+    public Lecture getSelectedLecture() {
+        return getSession().getSelectedLecture();
+    }
+
+    /**
      * Setzt die anzuzeigende Lehrveranstaltung auf den gegebenen Wert.
      *
      * @param pLecture
